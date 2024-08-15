d51dddf02239336263a959220619ffe739b9781b
Alex Buly <asbuly@icloud.com>
2024-08-14 23:29:56 -0400

    Adds buttons to toggle menu

--- script.js
+++ script.js
@@ -13,3 +13,47 @@ function Dropdown() {
 
     return {openMenu, closeMenu}
 }
+
+function ButtonEvents() {
+    const dropBtn = Dropdown();
+
+    const drop1 = () => {
+        const drop = document.querySelector(".drop");
+        const toggleOpen = document.querySelector(".toggleOpen");
+        const toggleClose = document.querySelector(".toggleClose");
+
+        drop.style.display = "none";
+        toggleClose.style.display = "none";
+
+        toggleOpen.addEventListener("click", () => {
+            dropBtn.openMenu(drop, toggleOpen, toggleClose);
+        });
+
+        toggleClose.addEventListener("click", () => {
+            dropBtn.closeMenu(drop, toggleClose, toggleOpen);
+        });
+    }
+
+    const drop2 = () => {
+        const secondDrop = document.querySelector(".drop2");
+        const linksOpen = document.querySelector(".linksOpen");
+        const linksClose = document.querySelector(".linksClose");
+
+        secondDrop.style.display = "none";
+        linksClose.style.display = "none";
+
+        linksOpen.addEventListener("click", () => {
+            dropBtn.openMenu(secondDrop, linksOpen, linksClose);
+        });
+
+        linksClose.addEventListener("click", () => {
+            dropBtn.closeMenu(secondDrop, linksClose, linksOpen);
+        });
+    }
+
+    return {drop1, drop2}
+}
+
+const btns = ButtonEvents();
+btns.drop1();
+btns.drop2();
